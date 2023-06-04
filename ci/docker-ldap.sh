f8a035cc7b7424960190a2cee409fd52a80d9006
Erik Sundell <erik.i.sundell@gmail.com>
2023-06-04 21:53:05 +0200

    Update CONTRIBUTING.md and associated docker-ldap.sh script

--- ci/docker-ldap.sh
+++ ci/docker-ldap.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-# source this file to setup LDAP
-# for local testing (as similar as possible to docker)
-
 set -e
 
-NAME="hub-test-ldap"
-DOCKER_RUN="docker run -d --name $NAME"
-RUN_ARGS="-p 389:10389 -p 636:10636 rroemhild/test-openldap"
-
-docker rm -f "$NAME" 2>/dev/null || true
-
-$DOCKER_RUN $RUN_ARGS
+# This file (re-)starts an openldap server to test against within a docker
+# container based on the image rroemhild/test-openldap.
+#
+# ref: https://github.com/rroemhild/docker-test-openldap
+# ref: https://hub.docker.com/r/rroemhild/test-openldap/
+#
+# Stop any existing test-openldap container
+docker rm --force test-openldap 2>/dev/null || true
+# Start a container, and expose some ports, where 389 and 636 are the local
+# system's ports that are redirected to the started container.
+#
+# - 389:10389 (ldap)
+# - 636:10636 (ldaps)
+#
+docker run --detach --name=test-openldap -p 389:10389 -p 636:10636 rroemhild/test-openldap:2.1
