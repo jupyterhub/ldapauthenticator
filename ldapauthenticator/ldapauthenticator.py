e7180c949bb1e8b90ed10662c3cc16b967b07b82
Erik Sundell <erik.i.sundell@gmail.com>
2024-09-20 00:05:24 +0200

    docs: add missing docs for `search_filter` and `attributes`

--- ldapauthenticator/ldapauthenticator.py
+++ ldapauthenticator/ldapauthenticator.py
@@ -297,16 +297,27 @@ class LDAPAuthenticator(Authenticator):
         help="""
         LDAP3 Search Filter to limit allowed users.
 
-        Matching the search_filter is necessary but not sufficient to grant access.
-        Grant access by setting one or more of `allowed_users`,
-        `allow_all`, `allowed_groups`, etc.
+        That a unique LDAP user is identified with the search_filter is
+        necessary but not sufficient to grant access. Grant access by setting
+        one or more of `allowed_users`, `allow_all`, `allowed_groups`, etc.
 
         Users who do not match this filter cannot be allowed
         by any other configuration.
+
+        The search filter string will be expanded, so that:
+
+        - `{userattr}` is replaced with the `user_attribute` config's value.
+        - `{username}` is replaced with an escaped username, either provided
+          directly or previously looked up with `lookup_dn` configured.
         """,
     )
 
-    attributes = List(config=True, help="List of attributes to be searched")
+    attributes = List(
+        config=True,
+        help="""
+        List of attributes to be passed in the LDAP search with `search_filter`.
+        """,
+    )
 
     auth_state_attributes = List(
         config=True,
