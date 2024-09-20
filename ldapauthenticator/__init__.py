d37b6f150ccf1ef5a06a3f6684543c08b25cde14
Erik Sundell <erik@sundellopensource.se>
2024-09-20 20:25:41 +0200

    Bump to 2.0.0b1

--- ldapauthenticator/__init__.py
+++ ldapauthenticator/__init__.py
@@ -3,4 +3,4 @@ from ldapauthenticator.ldapauthenticator import LDAPAuthenticator  # noqa
 # __version__ should be updated using tbump, based on configuration in
 # pyproject.toml, according to instructions in RELEASE.md.
 #
-__version__ = "2.0.0.dev"
+__version__ = "2.0.0b1"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="jupyterhub-ldapauthenticator",
-    version="2.0.0.dev",
+    version="2.0.0b1",
     description="LDAP Authenticator for JupyterHub",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
