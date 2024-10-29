7638a6e65754ee6103586c9dd5696b27ba730c9b
Simon Li <orpheus+devel@gmail.com>
2024-10-29 17:18:09 +0000

    Bump to 2.0.2.dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="jupyterhub-ldapauthenticator",
-    version="2.0.1",
+    version="2.0.2.dev",
     description="LDAP Authenticator for JupyterHub",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
